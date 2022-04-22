72887b2274613d269c806a37ea5bcf274e26ecba
heartandkarma <heartandkarma@gmail.com>
2022-04-21 18:39:41 -0700

    wheel reducer and actions setup, buttons hooked up for wheel

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,20 +1,36 @@
-import React from 'react'
-
-export default function Wheel(props) {
+import React from "react";
+import { connect } from "react-redux";
+import * as actions from "../state/action-creators";
+function Wheel(props) {
+  const handleClockwise = () => {
+    props.moveClockwise();
+  };
+  const handleCounterClockwise = () => {
+    props.moveCounterClockwise();
+  };
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
+        <div className="cog active" style={{ "--i": 0 }}>
+          B
+        </div>
         <div className="cog" style={{ "--i": 1 }}></div>
         <div className="cog" style={{ "--i": 2 }}></div>
         <div className="cog" style={{ "--i": 3 }}></div>
         <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+        <div className="cog" style={{ "--i": 5 }}></div>
+        {/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
-        <button id="counterClockwiseBtn" >Counter clockwise</button>
-        <button id="clockwiseBtn">Clockwise</button>
+        <button id="counterClockwiseBtn" onClick={handleCounterClockwise}>
+          Counter clockwise
+        </button>
+        <button id="clockwiseBtn" onClick={handleClockwise}>
+          Clockwise
+        </button>
       </div>
     </div>
-  )
+  );
 }
+
+export default connect((state) => state, actions)(Wheel);
